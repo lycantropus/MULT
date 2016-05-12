1b7228304d753473852c693582a4090c29bff576
Andre morais <mechanical.maggot.0@gmail.com>
2016-05-12 16:10:26 +0100

    a brincar com JS

---
+++ includes/scripts/audio.js
@@ -0,0 +1,16 @@
+
+
+room.onLoad = function(){
+room.createObject("Object",{text: "Testing",
+							js_id:"testezao",
+							id:"cube",
+							scale:Vector(.4,.08,.4),
+							pos: Vector(3,0, 7),
+							xdir: Vector(-1,0,0),
+							ydir: Vector(0,-1,0),
+							zdir: Vector(0,0,1),
+							col: Vector(4,0,-7)}
+					);
+
+}
+
